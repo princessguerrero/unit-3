bda8490818ee75dc4d61fc91d0c888e4a1b84bc4
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-11-18 13:23:43 -0500

    updated using ES6

--- ES-6/Part1ex/Q4.js
+++ ES-6/Part1ex/Q4.js
@@ -1,10 +1,8 @@
-
-
-function forEachSum(arr) {
-    let sum = 0;
-     arr.forEach(num => {
-         sum += num
-     })
-     return sum
+const forEachSum = (arr) => {
+    var sum = 0;
+    arr.forEach(num => sum += num) 
+        return sum
     }
+
+console.log(forEachSum([1, 2, 3, 4, 5]))     
 
