ed6f7a612543cc26d9b283a89bbcb9ec6395223d
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-11-18 14:16:48 -0500

    removed return and used arrow notation

--- ES-6/Part1ex/Q10.js
+++ ES-6/Part1ex/Q10.js
@@ -1,5 +1,6 @@
-const sum = (arr) => arr.reduce((acc, num) => {
-    return acc + num
-})
-
+const sum = (arr) => arr.reduce((acc, num) => acc + num)
+    
 console.log(sum([2, 3, 4, 5]))
+//              [5, 4, 5]
+//              [9, 5]
+//              [14] 
