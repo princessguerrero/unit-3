394380382cf8310a2bbf4f3b28b6fdc97319dbf2
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-11-18 13:56:40 -0500

    added comments

--- ES-6/Part1ex/Q8.js
+++ ES-6/Part1ex/Q8.js
@@ -1,5 +1,8 @@
 const firstLetterUpper = (str) =>
+//to change it to an array
  str.split(' ')
+//.map to go through the array and take the element at [0], uppercase it, and add the rest of the word
+//by concatenating and using slice(1) then joining with a space.
     .map(word => word[0].toUpperCase() + word.slice(1))
     .join(' ')
 
